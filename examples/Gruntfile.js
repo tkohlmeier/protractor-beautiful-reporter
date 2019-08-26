2fbce7ab25da8dff5ac0604f20cf363a18b0bede
miller45 <rklein@rosen-group.com>
2019-08-26 16:28:59 +0200

    fixed: unsescape failed when combined.json was in main dir

--- examples/Gruntfile.js
+++ examples/Gruntfile.js
@@ -54,24 +54,29 @@ module.exports = function (grunt) {
     grunt.registerTask('update', ['shell:npm_install']);
     grunt.registerTask('unescape-combined-json', () => {
         const CircularJSON = require('circular-json');
-
+        function doUnEscape(jsonFile,entry){
+            const text = fs.readFileSync(jsonFile).toString();
+            if (text && text[0] === `"`) {
+                console.log(`unescaped combined.json in '${entry}'`);
+                const cs = CircularJSON.parse(text);
+                const obj = JSON.parse(cs);
+                const str = JSON.stringify(obj, null, 4);
+                fs.writeFileSync(jsonFile, str);
+            } else { //else do nothing as the file is already unescaped
+                console.log(`doing nothing combined json in '${entry}' is already unescaped`);
+            }
+        }
         const dirContents = fs.readdirSync(reportsTmpPath);
         for (let entry of dirContents) {
             let fpath = path.resolve(reportsTmpPath, entry);
             if (fs.statSync(fpath).isDirectory()) {
-                const jsonFile=path.join(fpath, 'combined.json');
-                const text = fs.readFileSync(jsonFile).toString();
-
-                if (text && text[0] === `"`) {
-                    console.log(`unescaped combined.json in '${entry}'`);
-                    const cs = CircularJSON.parse(text);
-                    const obj = JSON.parse(cs);
-                    const str = JSON.stringify(obj, null, 4);
-                    fs.writeFileSync(jsonFile,str);
-                } else {//else do nothing as the file is already unescaped
-                    console.log(`doing nothing combined json in '${entry}' is already unescaped`);
+                const jsonFile = path.join(fpath, 'combined.json');
+                if (fs.existsSync(jsonFile)) {
+                    doUnEscape(jsonFile,entry);
                 }
 
+            } else if (path.basename(fpath) === 'combined.json') {
+                doUnEscape(fpath,entry);
             }
         }
 
